adba33341633902e95e92df47b81925ef4e8bc2b
Arthur <arthurakhatov91@gmail.com>
2019-02-05 00:44:39 +0500

    actions

--- src/containers/App.js
+++ src/containers/App.js
@@ -3,27 +3,36 @@ import { connect } from 'react-redux'
 import { User } from '../components/User'
 import { Page } from '../components/Page'
 import './App.css'
+import * as actions from '../actions/PageActions'
 
 class App extends Component {
   render() {
-    const { user, page } = this.props
+    const { user, page, setYearAction } = this.props
     return (
       <div className="App">
         <header>
           <h1 className="App-title">Мой топ фото</h1>
         </header>
         <User name={user.name} />
-        <Page photos={page.photos} year={page.year} />
+        <Page photos={page.photos} year={page.year} setYear={setYearAction} />
       </div>
     )
   }
 }
 
 const mapStateToProps = store => {
+  console.log(store)
   return {
     page: store.page,
     user: store.user,
   }
 }
 
-export default connect(mapStateToProps)(App)
+const mapDispatchToProps = dispatch => ({
+  setYearAction: year => dispatch(actions.setYear(year)),
+})
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(App)
