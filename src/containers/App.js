493bb26f9ea1c50dde7efae193ba849c57b226da
Arthur <arthurakhatov91@gmail.com>
2019-02-05 00:16:39 +0500

    containers & components

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,27 +1,25 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import { User } from '../components/User'
+import { Page } from '../components/Page'
 import './App.css'
 
 class App extends Component {
   render() {
-    const { name } = this.props.user
-    const { year, photos } = this.props.page
+    const { user, page } = this.props
     return (
-      <div>
+      <div className="App">
         <header>
           <h1 className="App-title">Мой топ фото</h1>
         </header>
-        <p>Привет {name}</p>
-        <p>
-          У тебя {photos.length} фото за {year} год
-        </p>
+        <User name={user.name} />
+        <Page photos={page.photos} year={page.year} />
       </div>
     )
   }
 }
 
 const mapStateToProps = store => {
-  console.log(store)
   return {
     page: store.page,
     user: store.user,
